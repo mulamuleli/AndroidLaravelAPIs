8d51fc1f4548033f4469bf2c189ba1b690933697
mulamuleli <rabulamx@legal.regn.net>
2021-02-18 11:19:21 +0200

    updated apis

--- routes/api.php
+++ routes/api.php
@@ -12,12 +12,23 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+// User
 Route::post('login','API\AuthController@Login');
 Route::post('register', 'API\AuthController@Register');
 Route::get('logout','API\AuthController@Logout');
 
+// Posts
 Route::post('post/create','API\PostController@create')->middleware('jwtAuth');
 Route::post('post/delete','API\PostController@delete')->middleware('jwtAuth');
 Route::post('post/update','API\PostController@update')->middleware('jwtAuth');
 Route::get('posts','API\PostController@posts')->middleware('jwtAuth');
 // Route::get('posts/my_posts','Api\PostsController@myPosts')->middleware('jwtAuth');
+
+//comment
+Route::post('comments/create','Api\CommentController@create')->middleware('jwtAuth');
+Route::post('comments/delete','Api\CommentController@delete')->middleware('jwtAuth');
+Route::post('comments/update','Api\CommentController@update')->middleware('jwtAuth');
+Route::post('posts/comments','Api\CommentController@comments')->middleware('jwtAuth');
+
+// likes
+Route::post('posts/like','Api\LikesController@like')->middleware('jwtAuth');
